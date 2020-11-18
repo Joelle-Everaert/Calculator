26c3bb80ad251a4417e291712a19f6edf0eab75f
Joelle Everaert <72984240+Joelle-Everaert@users.noreply.github.com>
2020-11-18 11:59:47 +0100

    ajout function %

--- script/calculator.js
+++ script/calculator.js
@@ -7,26 +7,34 @@ let linebreak = "<br>"; //creation linebreak pour la rappelé dans l'histo
 const btn = document.querySelectorAll('.btn')
 // on crée une variable qui selection tout les div btn de notre generateCalculator
 
+function pourcentage() {
+    return (calcul.textContent / 100)
+}
 
 for (let b of btn) {
     b.addEventListener('click', function () {
         if (b.textContent !== 'AC' && b.textContent !== '=' && b.textContent !== '%') {
             if (resultat.textContent == '') {
-                calcul.textContent += String(b.textContent); //si resultat est = a rien alors encode le contenu de mes boutons
+                calcul.textContent += (b.textContent); //si resultat est = a rien alors encode le contenu de mes boutons
             } else {
-                calcul.textContent = String(b.textContent); // sinon calcul = la valeur du bouton cliqué, et resultat = a rien
+                calcul.textContent = (b.textContent); // sinon calcul = la valeur du bouton cliqué, et resultat = a rien
                 resultat.textContent = ''
             }
 
-        } else if (b.textContent == '%') { // % avant car si on le met après, n'entre pas dans la boucle !! 
-            calcul.textContent += ' % ' + safeEval(String(resultat.textContent)) / 100; //On prend la somme pour l'afficher et la / 100
-            resultat.textContent = safeEval(String(resultat.textContent)) / 100; // juste + car on assigne pas la valeur a nouveau
+        } else if (b.textContent == '%') {
+            let pourcent = pourcentage();
+            calcul.textContent = calcul.textContent + '%'
+            resultat.textContent = pourcent;
+
         } else if (b.textContent == 'AC') {
             calcul.textContent = '';
             resultat.textContent = '';
+            
         } else if (b.textContent == '=') {
-            resultat.textContent = safeEval(String(calcul.textContent))
+            resultat.textContent = safeEval(calcul.textContent)
             historique.innerHTML += calcul.textContent + '=' + resultat.textContent + linebreak;
+
+
         }
     })
 }
